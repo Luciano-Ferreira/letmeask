dfb1d1494532762b4d2f0c0e1eaee910f7b3e827
Luciano Ferreira <luciano2023silva@gmail.com>
2021-06-22 19:15:46 -0300

    New Room page

---
+++ src/pages/NewRoom/index.tsx
@@ -0,0 +1,35 @@
+import illustrationImg from '../../assets/images/illustration.svg';
+import logoImg from '../../assets/images/logo.svg';
+import { Button } from '../../components/Button';
+
+import './styles.scss';
+
+export function NewRoom() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração sikmbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiencia em tempo real.</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <h2>Criar uma nova sala</h2>
+                    <form >
+                        <input 
+                            type="text"
+                            placeholder="Nome da sala"
+                        />
+                        <Button type="submit">
+                            Criar sala
+                        </Button>
+                    </form>
+                    <p>
+                        Quer entrar em uma sala existente <a href="/">Clique aqui</a>
+                    </p>
+                </div>
+            </main>
+        </div>
+    )
+}
