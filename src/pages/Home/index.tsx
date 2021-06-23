5c75ead90d5a2ec2cf419d284adaa41a262ca102
Luciano Ferreira <luciano2023silva@gmail.com>
2021-06-23 12:54:44 -0300

    joying a room

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import illustrationImg from '../../assets/images/illustration.svg';
@@ -6,14 +7,17 @@ import googleIconImg from '../../assets/images/google-icon.svg';
 
 import { Button } from '../../components/Button';
 
+import { database } from '../../services/firebase';
+import { useAuth } from '../../hooks/useAuth';
 
 import './styles.scss';
-import { useAuth } from '../../hooks/useAuth';
 
 export function Home() {
     const history = useHistory();
 
     const { signInWithGoogle, user } = useAuth();
+
+    const [ roomCode, setRoomCode ] = useState('');
 
     async function handleCreateRoom() {
         if (!user) {
@@ -22,6 +26,23 @@ export function Home() {
         
 
         history.push('/rooms/new');
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exist!')
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`)
     }
 
     return (
@@ -39,10 +60,12 @@ export function Home() {
                         Crie sua sala com Google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
-                    <form >
+                    <form onSubmit={handleJoinRoom}>
                         <input 
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
