d9ec67665b01be41870d888111b2b514883a1402
Luciano Ferreira <luciano2023silva@gmail.com>
2021-06-23 18:13:39 -0300

    take code from firebase and shows to user

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,15 +1,26 @@
+import { useParams } from 'react-router-dom';
+
 import logoImg from '../../assets/images/logo.svg';
 import { Button } from '../../components/Button';
+import { RoomCode } from '../../components/RoomCode';
 
 import './styles.scss';
 
+type RoomParams = {
+	id: string;
+}
+
 export function Room() {
+	const params = useParams<RoomParams>();
+
+
+
 	return (
 		<div id="page-room">
 			<header>
 				<div className="content">
 					<img src={logoImg} alt="Let me ask" />
-					<div>codigo da sala</div>
+					<RoomCode code={params.id}/>
 				</div>
 			</header>
 
