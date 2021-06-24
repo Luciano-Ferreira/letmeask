5487af708a75e934485361ee5ddfb6dbf501cd3e
Luciano Ferreira <luciano2023silva@gmail.com>
2021-06-24 18:29:26 -0300

    closing room

--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -1,13 +1,14 @@
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 import { Button } from '../../components/Button';
 import { RoomCode } from '../../components/RoomCode';
 import { Question } from '../../components/Question';
 import { useRoom } from '../../hooks/useRoom';
-
-
+import { database } from '../../services/firebase';
 
 import logoImg from '../../assets/images/logo.svg';
+import deleteImg from '../../assets/images/delete.svg';
+
 import './styles.scss';
 
 
@@ -16,10 +17,25 @@ type RoomParams = {
 }
 
 export function AdminRoom() {	
+	const history = useHistory();
 	const params = useParams<RoomParams>();
 	
 	const roomId = params.id;
 	const { title, questions } = useRoom(roomId);
+
+	async function handleEndRoom() {
+		database.ref(`rooms/${roomId}`).update({
+			endedAt: new Date(),
+		})
+
+		history.push('/')
+	}
+
+	async function handleDeleteQuestion(questionId: string) {
+		if (window.confirm('Você tem certeza que você deseja remover esta pergunta?')) {
+			await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+		}
+	}
 
 
 	return (
@@ -29,7 +45,12 @@ export function AdminRoom() {
 					<img src={logoImg} alt="Let me ask" />
 					<div>
             <RoomCode code={roomId}/>
-            <Button isOutlined>Encerrar sala</Button>
+            <Button 
+							isOutlined
+							onClick={handleEndRoom}
+						>
+							Encerrar sala
+						</Button>
           </div>
 				</div>
 			</header>
@@ -47,7 +68,12 @@ export function AdminRoom() {
 								content={question.content}
 								author={question.author}
 							>
-								
+								<button
+									type="button"
+									onClick={() => handleDeleteQuestion(question.id)}
+								>
+									<img src={deleteImg} alt="Remover pergunta" />
+								</button>
 							</Question>
 						)
 					})}
